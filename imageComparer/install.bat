2df08a69b4d711f9b49cf5630038dcd6e021ce81
Daniel Wong <drojfjord@gmail.com>
2019-06-08 14:58:44 +1000

    Sort sprites by ps3 name alphabetically in csv file and put in imageComparer directory

---
+++ imageComparer/install.bat
@@ -0,0 +1,27 @@
+setlocal
+
+:: Fix if batch file is run as administrator
+pushd %~dp0
+
+@echo off
+
+:: Print whether the user already has python installed on path
+where python >nul 2>&1
+if errorlevel 1 (
+    echo INFORMATION: No Python found on path
+) else (
+    echo WARNING: An existing Python was found on path
+)
+
+:: Running with -E disables checking of environment variables for python libs
+:: This should prevent conflicts with any Python dists already on the user's computer.
+echo INFORMATION: Using bundled Python to run installer
+"python/python.exe" -E compareImages.py
+
+echo ----------------------------------------------------------- 
+echo ------------ Batch file has finished executing ------------
+echo ------------ Press any key to close this window -----------  
+echo ----------------------------------------------------------- 
+pause
+
+popd
