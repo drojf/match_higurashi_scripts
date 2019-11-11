c9a132be8dd39c3bb0f33efd5ae9e373612b5579
Daniel Wong <drojfjord@gmail.com>
2019-11-11 22:58:41 +1100

    modify match_higurashi_sprites for upcoming background matching

--- match_higurashi_sprites.py
+++ match_higurashi_sprites.py
@@ -9,106 +9,117 @@ from match_statistics import MatchStatistics
 from match_statistics_to_csv import convertMatchingToCSV
 from matching_core import get_matching_script_paths_between_folders, update_match_statistics, MatchConfiguration
 
-steamBustRegex = re.compile(r'(DrawBust|ChangeBust)[^\(]+\(\s*\d*\s*,\s*\"([^\"]+)')
-ps3BustRegex = re.compile(r'ModDraw[^\(]+\(\s*\d*\s*,\s*\d*\s*,\s*\"([^\"]+)')
+def doSpriteMatching():
+	steamBustRegex = re.compile(r'(DrawBust|ChangeBust)[^\(]+\(\s*\d*\s*,\s*\"([^\"]+)')
+	ps3BustRegex = re.compile(r'ModDraw[^\(]+\(\s*\d*\s*,\s*\d*\s*,\s*\"([^\"]+)')
 
-class MyParser(argparse.ArgumentParser):
-	def error(self, message):
-		sys.stderr.write('error: %s\n' % message)
-		self.print_help()
-		sys.exit(2)
+	class MyParser(argparse.ArgumentParser):
+		def error(self, message):
+			sys.stderr.write('error: %s\n' % message)
+			self.print_help()
+			sys.exit(2)
 
-def write_to_file(text : str, path : str):
-	with open(path, 'w', encoding='utf-8') as output_file:
-		output_file.write(text)
+	def write_to_file(text : str, path : str):
+		with open(path, 'w', encoding='utf-8') as output_file:
+			output_file.write(text)
 
-def normalize_time_of_day_and_portrait(ps3_image_path : str) -> str:
-	return ps3_image_path.replace('/normal/','/').replace('/sunset/', '/').replace('/night/','/').replace('portrait/','sprite/')
-
-
-def keep_only_ryushiki_ps3(ps3_name):
-	raise NotImplemented()
+	def normalize_time_of_day_and_portrait(ps3_image_path : str) -> str:
+		return ps3_image_path.replace('/normal/','/').replace('/sunset/', '/').replace('/night/','/').replace('portrait/','sprite/')
 
 
-match_statistics = MatchStatistics()
-reverse_match_statistics = MatchStatistics()
-output_path = 'noconsole_output.txt'
+	def keep_only_ryushiki_ps3(ps3_name):
+		raise NotImplemented()
 
-NO_CONSOLE = True
-if NO_CONSOLE:
-	for episode_num in range(1, 7):
+
+	match_statistics = MatchStatistics()
+	reverse_match_statistics = MatchStatistics()
+	output_path = 'noconsole_output.txt'
+
+	NO_CONSOLE = True
+	if NO_CONSOLE:
+		for episode_num in range(1, 7):
+			config = MatchConfiguration(steamBustRegex, ps3BustRegex)
+			config.ps3_name_modification_function = normalize_time_of_day_and_portrait
+			config.ps3_whitelist_function = None
+
+			matching_script_paths = get_matching_script_paths_between_folders(f'steam_scripts\\ep{episode_num}', f'ps3_scripts\\ep{episode_num}')
+			for steam_script_path, ps3_script_path in matching_script_paths:
+				update_match_statistics(match_statistics,
+										reverse_match_statistics,
+										steam_script_path,
+										ps3_script_path,
+										match_configuration=config)
+	else:
+		parser = MyParser(description='Match sprites between steam and ps3 scripts, given two input folders containing scripts as .txt files.')
+		parser.add_argument('steam_scripts_folder', type=str, help='path containing steam scripts as .txt files')
+		parser.add_argument('ps3_scripts_folder', type=str, help='path containing ps3 scripts as .txt files')
+		parser.add_argument('output_file_path', type=str, help='name of file where results are written (JSON format)')
+		parser.add_argument('--ignore_time_of_day_and_portrait', type=bool, default=False, help='ignore "normal", "sunset", "night", "portrait" in ps3 paths')
+		parser.add_argument('--whitelist', type=bool, default=False, help='whitelist ps3 names')
+
+		args = parser.parse_args()
+
+		output_path = args.output_file_path
+
+		ps3_filter_function = None
+		if args.ignore_time_of_day:
+			print("NOTE: ignoring time of day")
+			ps3_filter_function = normalize_time_of_day_and_portrait
+		else:
+			print("NOTE: Keeping time of day")
+
+		ps3_whitelist_function = None
+		if args.whitelist:
+			print("NOTE: whitelist not applied")
+			ps3_whitelist_function = keep_only_ryushiki_ps3
+		else:
+			print("NOTE: applying whitelist")
+
 		config = MatchConfiguration(steamBustRegex, ps3BustRegex)
-		config.ps3_name_modification_function = normalize_time_of_day_and_portrait
-		config.ps3_whitelist_function = None
+		config.ps3_name_modification_function = ps3_filter_function
+		config.ps3_whitelist_function = ps3_whitelist_function
 
-		matching_script_paths = get_matching_script_paths_between_folders(f'steam_scripts\\ep{episode_num}', f'ps3_scripts\\ep{episode_num}')
+		matching_script_paths = get_matching_script_paths_between_folders(args.steam_scripts_folder, args.ps3_scripts_folder)
 		for steam_script_path, ps3_script_path in matching_script_paths:
 			update_match_statistics(match_statistics,
 									reverse_match_statistics,
 									steam_script_path,
 									ps3_script_path,
 									match_configuration=config)
+
+
+	print("\n\n----------------------------------------------")
+	y = convertMatchingToCSV(match_statistics)
+	for x in y:
+		print(x)
+	write_to_file('\n'.join(y), output_path + '.csv')
+
+	print("\n\n----------------------------------------------")
+	y = convertMatchingToCSV(reverse_match_statistics)
+	for x in y:
+		print(x)
+	write_to_file('\n'.join(y), output_path + '.reversed.csv')
+
+
+	json_string = json.dumps(match_statistics.statistics, sort_keys=True, indent=4)
+
+	print(json_string)
+	write_to_file(json_string, output_path)
+
+	#dump reverse statistics
+	json_string = json.dumps(reverse_match_statistics.statistics, sort_keys=True, indent=4)
+
+	print(json_string)
+	write_to_file(json_string, output_path + '.reversed.txt')
+
+	#normal night sunset
+
+def doBackgroundMatching():
+	pass
+
+matching_type = "background"
+
+if matching_type == "background":
+	doBackgroundMatching()
 else:
-	parser = MyParser(description='Match sprites between steam and ps3 scripts, given two input folders containing scripts as .txt files.')
-	parser.add_argument('steam_scripts_folder', type=str, help='path containing steam scripts as .txt files')
-	parser.add_argument('ps3_scripts_folder', type=str, help='path containing ps3 scripts as .txt files')
-	parser.add_argument('output_file_path', type=str, help='name of file where results are written (JSON format)')
-	parser.add_argument('--ignore_time_of_day_and_portrait', type=bool, default=False, help='ignore "normal", "sunset", "night", "portrait" in ps3 paths')
-	parser.add_argument('--whitelist', type=bool, default=False, help='whitelist ps3 names')
-
-	args = parser.parse_args()
-
-	output_path = args.output_file_path
-
-	ps3_filter_function = None
-	if args.ignore_time_of_day:
-		print("NOTE: ignoring time of day")
-		ps3_filter_function = normalize_time_of_day_and_portrait
-	else:
-		print("NOTE: Keeping time of day")
-
-	ps3_whitelist_function = None
-	if args.whitelist:
-		print("NOTE: whitelist not applied")
-		ps3_whitelist_function = keep_only_ryushiki_ps3
-	else:
-		print("NOTE: applying whitelist")
-
-	config = MatchConfiguration(steamBustRegex, ps3BustRegex)
-	config.ps3_name_modification_function = ps3_filter_function
-	config.ps3_whitelist_function = ps3_whitelist_function
-
-	matching_script_paths = get_matching_script_paths_between_folders(args.steam_scripts_folder, args.ps3_scripts_folder)
-	for steam_script_path, ps3_script_path in matching_script_paths:
-		update_match_statistics(match_statistics,
-								reverse_match_statistics,
-								steam_script_path,
-								ps3_script_path,
-								match_configuration=config)
-
-
-print("\n\n----------------------------------------------")
-y = convertMatchingToCSV(match_statistics)
-for x in y:
-	print(x)
-write_to_file('\n'.join(y), output_path + '.csv')
-
-print("\n\n----------------------------------------------")
-y = convertMatchingToCSV(reverse_match_statistics)
-for x in y:
-	print(x)
-write_to_file('\n'.join(y), output_path + '.reversed.csv')
-
-
-json_string = json.dumps(match_statistics.statistics, sort_keys=True, indent=4)
-
-print(json_string)
-write_to_file(json_string, output_path)
-
-#dump reverse statistics
-json_string = json.dumps(reverse_match_statistics.statistics, sort_keys=True, indent=4)
-
-print(json_string)
-write_to_file(json_string, output_path + '.reversed.txt')
-
-#normal night sunset
+	doSpriteMatching()
