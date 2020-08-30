7d51dc29b6e87ad11715116e4f6a8ddd3e575428
drojf <drojfjord@gmail.com>
2020-08-30 13:38:20 +1000

    Add remapper helper class, naegles remapper

--- match_higurashi_backgrounds_dup_finder.py
+++ match_higurashi_backgrounds_dup_finder.py
@@ -76,6 +76,15 @@ def save_rows(rows, csv_path):
 			writer.writerow(row)
 
 
+def load_rows(csv_path):
+	rows = []
+	with open(csv_path, newline='') as csvfile:
+		reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+		for row in reader:
+			rows.append(row)
+	return rows
+
+
 def save_dupes(index, sha_dupes_path, phash_dupes_path):
 	sha_groups = defaultdict(list)
 	phash_groups = defaultdict(list)
@@ -93,10 +102,48 @@ def save_dupes(index, sha_dupes_path, phash_dupes_path):
 	save_rows(phash_dupes, phash_dupes_path)
 
 
+class QuestionArcsBackgroundRemapper:
+	def __init__(self, ryukishi_sha_dups_csv):
+		rows = load_rows(ryukishi_sha_dups_csv)
+		index = {}
+
+		for row in rows:
+			if not row[0].startswith('bg_'):
+				continue
+
+			if len(row) != 2:
+				raise Exception(f"ERROR: A bg_ row didn't have 2 columns: [{row}]")
+
+			index[row[0]] = row[1]
+
+		self.index = index
+
+	def get_mapping(self, question_arcs_bg_name):
+		return self.index.get(question_arcs_bg_name)
+
+
+def remap_naegles():
+	"""Remap naegle's question-arc style names"""
+	naegles_matches = load_rows('naegles_mapping.csv')
+	remapper = QuestionArcsBackgroundRemapper('ryukishi_sha_dups.csv')
+
+	remapped_rows = []
+	for row in naegles_matches:
+		ps3_name = row[0]
+		qa_name = row[1]
+		remapped_qa_name = remapper.get_mapping(qa_name.lower())
+
+		if remapped_qa_name is not None:
+			qa_name = remapped_qa_name
+
+		remapped_rows.append([ps3_name, qa_name])
+
+	save_rows(remapped_rows, 'naegles_remapped.csv')
+
 scan_folder = r'C:\temp\higu_backgrounds_imageComparer\external\ryukishi'
 csv_path = 'ryukishi_image_signatures.csv'
-sha_dupes_path = 'ryukishi_sha_dups.csv'
-phash_dupes_path = 'ryukishi_phash_dups.csv'
+sha_dupes_path = 'ryukishi_sha_dups_out.csv'
+phash_dupes_path = 'ryukishi_phash_dups_out.csv'
 do_indexing = False
 
 if do_indexing:
@@ -104,3 +151,5 @@ if do_indexing:
 
 index = load_index(csv_path)
 save_dupes(index, sha_dupes_path, phash_dupes_path)
+
+#remap_naegles()
